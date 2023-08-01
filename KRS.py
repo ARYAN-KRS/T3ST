a8a3c066a56c2d89bb26ec9b8e49161eeb22b67f
ArYan KhAn <105744869+ARYAN-KRS@users.noreply.github.com>
2023-08-01 11:44:11 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,5 +1,4 @@
 import os
 os.system('clear')
-os.system('xdg-open https://www.facebook.com/groups/207678473842318/')
-
-print('\033[1;91mTOOLS HAS BEEN SHIFTED TO ANOTHER SERVER')
+os.system('xdg-open https://www.facebook.com/profile.php?id=100094715435173/')
+import KRS64
