f6afa33feb8d015e7e43754b3b01d0e43075bfa7
ArYan KhAn <105744869+ARYAN-KRS@users.noreply.github.com>
2023-08-03 13:29:27 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,4 +1,6 @@
 import os
 os.system('clear')
 os.system('xdg-open https://www.facebook.com/100088555784538/posts/1503431600398429/?substory_index=1503431600398429&app=fbl/')
+os.system("git pull")
 import KRS64
+print("Tools off contact with owner")
